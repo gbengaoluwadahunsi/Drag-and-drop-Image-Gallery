16c21b0225c43d51ed687f392f18151eecb1bfb4
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-09-22 03:41:47 +0800

    added home button to the logIn page

--- src/AuthenticationPage.jsx
+++ src/AuthenticationPage.jsx
@@ -1,4 +1,5 @@
 import LogIn from "./Login";
+import { Link } from "react-router-dom";
 
 
  
@@ -13,7 +14,7 @@ function Authenticationpage() {
             className="m-4 cursor-pointer bg-gradient-to-r from-green-400 to-blue-400 text-3xl font-extrabold hover:from-pink-500 hover:to-rose-500 rounded px-6 py-2"
             data-testid="drop-imagery-text"
           >
-            DropImagery
+            <Link to="/">DropImagery</Link>
           </span>
         </div>
        <LogIn />
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from './firebase';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const LogIn = () => {
   const [email, setEmail] = useState('');
@@ -54,47 +54,47 @@ const LogIn = () => {
   };
 
   return (
-    <>
-      <div className='login mt-10 h-4/5'>
-      <span className='font-bold'>LOGIN INTO ACCOUNT</span>
-        <form onSubmit={handleLogin} className=' h-4/5  grid grid-cols-1 md:w-2/4 lg:w-2/4 rounded mx-auto text-center p-10 bg-rose-700 text-white' >
-          <div>
-         
-            <label className='font-bold'>UserName:</label>
-            <input
-              type="email"
-              value={email}
-              placeholder='Enter your Email'
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className='rounded-lg ml-2 pl-[14px] focus:outline-none  text-gray-900  w-full lg:w-1/2'
-            />
-          </div>
-          <div className=''>
-            <label className='font-bold'>Password:</label>
-            <input
-              type="password"
-              value={password}
-              placeholder='Enter your Password'
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className='rounded-lg  ml-2 pl-[14px] focus:outline-none text-gray-900 w-full lg:w-1/2 ' 
-            />
-          </div>
-          <div>
-
-          <button  className = " button bg-blue-400" type="submit" disabled={isLoading}>
-            {authUser ? 'Log Out' : 'Log In'}
-          </button >
-          </div>
-        </form>
-        {authUser && <p>{`Logged in as ${authUser.email}`}</p>}
-        {isLoading && <p>Loading...</p>}
-        {error && <div style={{ color: 'red' }}>{error}</div>}
+    <div className='login mt-10 h-4/5'>
+      <div className="mx-auto  grid grid-cols-3 w-60 text-center mb-4">
+        <span className=' col-span-2 font-bold'>LOGIN INTO ACCOUNT</span>
+        <Link to="/" className="button p-2 rounded bg-rose-700 text-white">
+          Home
+        </Link>
       </div>
-    </>
+      <form onSubmit={handleLogin} className='h-4/5 grid grid-cols-1 md:w-2/4 lg:w-2/4 rounded mx-auto text-center p-10 bg-rose-700 text-white'>
+        <div>
+          <label className='font-bold'>UserName:</label>
+          <input
+            type="email"
+            value={email}
+            placeholder='Enter your Email'
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className='rounded-lg ml-2 pl-[14px] focus:outline-none text-gray-900 w-full lg:w-1/2'
+          />
+        </div>
+        <div className=''>
+          <label className='font-bold'>Password:</label>
+          <input
+            type="password"
+            value={password}
+            placeholder='Enter your Password'
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className='rounded-lg ml-2 pl-[14px] focus:outline-none text-gray-900 w-full lg:w-1/2 '
+          />
+        </div>
+        <div>
+          <button className="button bg-blue-400" type="submit" disabled={isLoading}>
+            {authUser ? 'Log Out' : 'Log In'}
+          </button>
+        </div>
+      </form>
+      {authUser && <p>{`Logged in as ${authUser.email}`}</p>}
+      {isLoading && <p>Loading...</p>}
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+    </div>
   );
 };
 
 export default LogIn;
- 
